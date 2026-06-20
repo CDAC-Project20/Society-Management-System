1c7bc0aacfe4c2058765cc1c1aeed6d0afa9fd62
Mukesh242004 <borolemukesh1@gmail.com>
2026-06-20 18:49:50 +0530

    update home page

--- my-app/src/components/HomeComp.jsx
+++ my-app/src/components/HomeComp.jsx
@@ -5,9 +5,12 @@ export default function HomeComp() {
   return (
     <div className="container mt-5">
       <h1 className="text-center mb-4">Welcome to Society Management System</h1>
-      <p className="text-center">Manage complaints, payments, polls, visitors, and notices all in one place.</p>
+      <p className="text-center">
+        Manage complaints, payments, polls, visitors, and notices all in one
+        place.
+      </p>
+      <h1>hello</h1>
       <FooterComp />
     </div>
   );
 }
-
